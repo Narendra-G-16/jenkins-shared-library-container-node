55f9414d0d50a6c49332e2d4c232b4b6d2b7442e
Narendra G <narendra@planetc.net>
2026-03-15 19:22:47 +0530

    Add package update and vim installation steps

--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,5 +1,16 @@
 def call() {
     echo "Executing Shared Library Stage"
+
+    sh '''
+        echo "Updating package list..."
+        sudo apt-get update -y
+
+        echo "Installing vim..."
+        sudo apt-get install -y vim
+
+        echo "Vim installation completed"
+    '''
+
     sh "date"
     sh "hostname"
 }
